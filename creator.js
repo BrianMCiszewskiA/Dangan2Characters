1b5930d547d3013749c2ac1400c81bb1ac5f2518
BrianMCiszewskiA <brianmciszewski@hotmail.com.ar>
2021-06-25 14:12:31 -0300

    modificada la barra de eleccion de personajes
    ahora es todo un html, y la barra de elegir personajes funciona bien (pendiente modificarla visualmente)

--- creator.js
+++ creator.js
@@ -1,7 +1,12 @@
 function createHtml(char) {
-  window.open("characterPage.html", "_self"); //esto hace q lo demas de abajo ya no aplique
   setActualPj(char);
   changePjPage(char);
-  //hay 2 opciones, encontrar una forma q esto funcione(? tipo enviando el parametro o algo idk, o, hacer q los primeros botones y la char page sean el mismo html
+  showCharPage();
+  eliminarElemento("charSelect");
+}
+
+function showCharPage(){
+	document.getElementById("headerBar").style.visibility = "visible";
+	document.getElementById("mainBox").style.visibility = "visible";
 }
 
